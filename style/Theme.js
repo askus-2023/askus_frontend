63015f65e87748a3abd5072c9944ec0cd20f6bc1
t-junne <108136143+t-junne@users.noreply.github.com>
2023-03-21 00:54:30 +0900

    Migration to Vite
    eslint, prettier 설정, Globalstyle, theme 설정

---
+++ style/Theme.js
@@ -0,0 +1,14 @@
+const colors = {
+  green50: '#3DD666',
+  green70: '#27B94E',
+  green90: '#23A445',
+  grey90: '#323232',
+  grey70: '#616161',
+  grey50: '#9E9E9E',
+  grey40: '#BDBDBD',
+  grey30: '#E0E0E0',
+  grey20: '#EEEEEE',
+  grey10: '#F5F5F5',
+}
+
+export const theme = { colors }
