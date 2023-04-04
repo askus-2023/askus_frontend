ed36a014f88d87cfc35f4308404de56c667b9a87
dlsdo1101 <dlsdo1101@naver.com>
2023-04-04 14:56:35 +0900

    프로필 및 프로필 수정 페이지 작업

---
+++ src/components/common/card/SquareCard.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { theme } from '../../../styles/Theme';
+import heartEmpty from '../../../assets/icons/heart-empty.svg';
+import heartFill from '../../../assets/icons/heart-fill.svg';
+
+const SquareCard = ({ thumbnail, menu, title, date, category, like }) => {
+  const [show, setShow] = useState(false);
+  const [isLike, setIsLike] = useState(like);
+
+  const iconHandler = () => {
+    if (isLike) {
+      setIsLike(!isLike);
+      // setTotal((total) => total - 1);
+    } else {
+      setIsLike(!isLike);
+      // setTotal((total) => total + 1);
+    }
+  };
+
+  const dateFormat =
+    date.getFullYear() + '.' + (date.getMonth() + 1) + '.' + date.getDate();
+
+  return (
+    <Wrapper
+      onMouseOver={() => setShow(true)}
+      onMouseOut={() => setShow(false)}
+    >
+      <img src={thumbnail} alt='thumbnail' className='thumbnail' />
+      {show && (
+        <div className='backcover'>
+          <div className='category'>
+            [{category}] {menu}
+          </div>
+          <div className='title'>{title}</div>
+          <div className='bottomsection'>
+            {!isLike ? (
+              <Like src={heartEmpty} alt='heartEmpty' onClick={iconHandler} />
+            ) : (
+              <Like src={heartFill} alt='heartFill' onClick={iconHandler} />
+            )}
+            <div className='category date'>{dateFormat}</div>
+          </div>
+        </div>
+      )}
+    </Wrapper>
+  );
+};
+
+export default SquareCard;
+
+const Wrapper = styled.div`
+  width: 30rem;
+  height: 30rem;
+  cursor: pointer;
+  margin: 1.3rem;
+  .thumbnail {
+    position: absolute;
+    width: 30rem;
+    height: 30rem;
+    z-index: -1;
+  }
+  &:hover {
+  }
+  .backcover {
+    background-color: rgba(0, 0, 0, 0.4);
+    color: ${theme.colors.grey10};
+    width: 30rem;
+    height: 30rem;
+    padding: 2.9rem;
+    font-size: 1.7rem;
+    .category {
+      margin-bottom: 3.5rem;
+      overflow: hidden;
+      display: -webkit-box;
+      -webkit-line-clamp: 1;
+      -webkit-box-orient: vertical;
+    }
+    .title {
+      font-size: 1.9rem;
+      line-height: 3rem;
+      overflow: hidden;
+      word-break: break-word;
+      text-overflow: ellipsis;
+      display: -webkit-box;
+      -webkit-line-clamp: 5;
+      -webkit-box-orient: vertical;
+      height: 15rem;
+      margin-bottom: 2rem;
+    }
+    .date {
+      display: flex;
+      flex-direction: row-reverse;
+    }
+    .bottomsection {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+    }
+  }
+`;
+
+const Like = styled.img`
+  width: 3rem;
+  height: 3rem;
+  /* object-fit: cover; */
+  cursor: pointer;
+  margin-bottom: 3.5rem;
+`;
---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,151 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { theme } from '../../styles/Theme';
+import profile from '../../assets/images/default-profile.png';
+import OutlinedButton from '../../components/common/button/OutlinedButton';
+import SquareCard from '../../components/common/card/SquareCard';
+import exPost from '../Board/DummyPost';
+
+const Profile = () => {
+  const [selectedCate, setSelectedCate] = useState('작성글');
+  const navigate = useNavigate();
+
+  const postcategory = ['작성글', '좋아요'];
+
+  const categoryHandler = (e) => {
+    setSelectedCate(e.target.value);
+  };
+
+  return (
+    <Wrapper>
+      <TopSection>
+        <Info>
+          <img src={profile} alt='profile' />
+          <div className='infotxt'>
+            <Greettxt>
+              <span>아무개</span> 님, 반가워요!
+            </Greettxt>
+            <div>abc@email.com</div>
+          </div>
+        </Info>
+        <div>
+          <OutlinedButton onClick={() => navigate('edit')}>
+            내 정보 수정
+          </OutlinedButton>
+        </div>
+      </TopSection>
+      <div>
+        <PostCategory>
+          {postcategory.map((cate, i) => (
+            <button
+              key={i}
+              className={selectedCate === cate ? 'cateSelect' : ''}
+              value={cate}
+              onClick={categoryHandler}
+            >
+              {cate}(2)
+            </button>
+          ))}
+        </PostCategory>
+        <PostCard>
+          {selectedCate === '좋아요'
+            ? exPost
+                .map((post) => (
+                  <SquareCard
+                    key={post.id}
+                    thumbnail={post.thumbnail}
+                    menu={post.menu}
+                    title={post.title}
+                    category={post.category}
+                    date={new Date(post.date)}
+                    like={post.like}
+                  />
+                ))
+                .filter((post) => post.props.like === true)
+            : exPost.map((post) => (
+                <SquareCard
+                  key={post.id}
+                  thumbnail={post.thumbnail}
+                  menu={post.menu}
+                  title={post.title}
+                  category={post.category}
+                  date={new Date(post.date)}
+                  like={post.like}
+                />
+              ))}
+        </PostCard>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Profile;
+
+const Wrapper = styled.div`
+  width: 89.6%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 4rem;
+  margin: 13rem auto auto;
+`;
+
+const TopSection = styled.div`
+  display: flex;
+  justify-content: space-around;
+  height: fit-content;
+  align-items: center;
+`;
+
+const Info = styled.div`
+  display: flex;
+  align-items: center;
+  img {
+    width: 12rem;
+    height: 12rem;
+    object-fit: cover;
+    border-radius: 6rem;
+  }
+  .infotxt {
+    margin-left: 2rem;
+    line-height: 3rem;
+    font-size: 1.5rem;
+    color: ${theme.colors.grey70};
+  }
+`;
+
+const Greettxt = styled.div`
+  font-size: 2.5rem;
+  color: ${theme.colors.grey90};
+  span {
+    font-weight: bold;
+  }
+`;
+
+const PostCategory = styled.div`
+  display: flex;
+  gap: 2rem;
+  border-bottom: 0.1rem solid gray;
+  margin: auto 19rem;
+  cursor: pointer;
+  button {
+    width: 10rem;
+    font-size: 2rem;
+    line-height: 5rem;
+    &:hover {
+      font-weight: 600;
+    }
+  }
+  .cateSelect {
+    font-weight: 600;
+  }
+`;
+
+const PostCard = styled.div`
+  display: flex;
+  margin: 2rem 19rem;
+  justify-content: flex-start;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
